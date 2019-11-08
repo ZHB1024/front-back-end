a250e53162d549733b94c9151bffc13e69708b52
ZHB1024 <zhb20111503@126.com>
2019-11-08 15:27:16 +0800

    return ResponseEntity

--- back-end/zhb-forever-server/zhb-forever-server-server/src/main/java/com/forever/zhb/server/server/controller/MsgController.java
+++ back-end/zhb-forever-server/zhb-forever-server-server/src/main/java/com/forever/zhb/server/server/controller/MsgController.java
@@ -1,6 +1,7 @@
 package com.forever.zhb.server.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,10 @@ public class MsgController {
     private MsApi msFeign;
     
     @GetMapping("/sendmail")
-    public void sendMail() {
+    public ResponseEntity<String> sendMail() {
         String result = msFeign.sendMail("开会通知");
         log.info(result);
+        return ResponseEntity.ok(result);
     }
 
 }
