11435c5790a691a9335b80b6317d5a78b6817ebb
ZHB1024 <zhb20111503@126.com>
2019-11-19 13:58:56 +0800

    config client

--- back-end/zhb-forever-springcloud-server/zhb-forever-server-server/src/main/java/com/forever/zhb/server/server/ServerApplication.java
+++ back-end/zhb-forever-springcloud-server/zhb-forever-server-server/src/main/java/com/forever/zhb/server/server/ServerApplication.java
@@ -2,12 +2,10 @@ package com.forever.zhb.server.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableDiscoveryClient
 public class ServerApplication {
     
     public static void main(String[] args) {
