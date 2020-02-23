b3468364ffb009f7946ab571974ed5ecfcd78664
ZHB1024 <zhb901024>
2020-02-23 20:21:45 +0800

    schedule-task

---
+++ aop-study/src/main/java/com/zhb/task/MultiThreadScheduleTask.java
@@ -0,0 +1,29 @@
+package com.zhb.task;
+
+import java.time.LocalDateTime;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling   // 1.开启定时任务
+@EnableAsync        // 2.开启多线程
+public class MultiThreadScheduleTask {
+    
+    @Async
+    @Scheduled(cron = "0 11 20 * * ?")  //间隔1秒
+    public void first() throws InterruptedException {
+        System.out.println("第一个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+    }
+
+//    @Async
+//    @Scheduled(fixedDelay = 1000)
+//    public void second() {
+//        System.out.println("第二个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+//        System.out.println();
+//    }
+
+}
